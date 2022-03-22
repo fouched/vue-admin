7ca6e2dc31a067c67e9c917633f988785743f6a3
Fouche du Preez <fouche@limehouse.co.za>
2022-03-22 19:12:35 +0200

    ignore eslint warning for chart

--- src/pages/MainDashboard.vue
+++ src/pages/MainDashboard.vue
@@ -1,50 +1,48 @@
 <template>
-  <h2 class="mt-3 mb-4">Daily Sales</h2>
-  <div id="chart"></div>
+	<h2 class="mt-3 mb-4">Daily Sales</h2>
+	<div id="chart"></div>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted} from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import * as c3 from 'c3'
-import axios from "axios";
+import axios from 'axios'
 
 export default defineComponent({
-  name: 'MainDashboard',
-  setup() {
+	name: 'MainDashboard',
+	setup() {
+		onMounted(async () => {
+			const chart = c3.generate({
+				bindto: '#chart',
+				data: {
+					x: 'x',
+					columns: [['x'], ['Sales']],
+					types: {
+						Sales: 'bar',
+					},
+				},
+				axis: {
+					x: {
+						type: 'timeseries',
+						tick: {
+							format: '%Y-%m-%d',
+						},
+					},
+				},
+			})
 
-    onMounted(async () => {
-      const chart = c3.generate({
-        bindto: '#chart',
-        data: {
-          x: 'x',
-          columns: [
-            ['x'],
-            ['Sales']
-          ],
-          types: {
-            Sales: 'bar'
-          }
-        },
-        axis: {
-          x: {
-            type: 'timeseries',
-            tick: {
-              format: '%Y-%m-%d'
-            }
-          }
-        }
-      })
-
-      const {data} = await axios.get('chart')
-      console.log(data)
-      chart.load({
-        columns: [
-          ['x', ...data.map((r: any) => r.date)],
-          ['Sales', ...data.map((r: any) => r.sum)]
-        ]
-      })
-    })
-  }
+			const { data } = await axios.get('chart')
+			console.log(data)
+			chart.load({
+				columns: [
+          // eslint-disable-next-line
+					['x', ...data.map((r: any) => r.date)],
+          // eslint-disable-next-line
+					['Sales', ...data.map((r: any) => r.sum)],
+				],
+			})
+		})
+	},
 })
 </script>
 
